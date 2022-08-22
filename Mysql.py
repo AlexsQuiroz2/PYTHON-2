ea47946a7d4e1127a3aba1b072ef2ed089cfbd9d
alexs <alexsquiroz054@gmail.com>
2022-08-22 17:03:32 -0500

    actualizacion midatabase

--- Mysql.py
+++ Mysql.py
@@ -4,12 +4,13 @@ mydb = mysql.connector.connect(
   host='localhost',
   user='root',
   password='admin',
+  database="midatabase"
 )
 
 mycursor = mydb.cursor()
-mycursor.execute("USE midatabase")
 
-
+'''mycursor.execute("CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))")'''
+'''mycursor.execute("ALTER TABLE customers ADD COLUMN id INT AUTO_INCREMENT PRIMARY KEY")'''
 mycursor.execute("SHOW TABLES")
 
 for x in mycursor:
