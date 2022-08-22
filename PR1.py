2abd097cd52a8210083f4a7ea9fab9732201ca8a
alexs <alexsquiroz054@gmail.com>
2022-08-22 15:06:41 -0500

    prueba 1

---
+++ PR1.py
@@ -0,0 +1,49 @@
+import numpy as np
+
+'''arr = np.array([1, 2, 3, 4], ndmin=5)
+
+print(arr)
+print('number of dimensions :', arr.ndim)'''
+
+'''
+a = np.array(42)
+b = np.array([1, 2, 3, 4, 5])
+c = np.array([[1, 2, 3], [4, 5, 6]])
+d = np.array([[[1, 2, 3], [4, 5, 6]], [[1, 2, 3], [4, 5, 6]]])
+
+print(a)
+print(b)
+print(c)
+print(d)
+
+print(a.ndim)
+print(b.ndim)
+print(c.ndim)
+print(d.ndim)'''
+
+'''arr = np.array([[1, 2, 3], [4, 5, 6]])
+
+print(arr[0:3])
+print(arr.ndim)'''
+
+
+'''arr = np.array([[1,2,3,4,5], [6,7,8,9,10]])
+
+print('5th element on 2nd row: ', arr[1, 4])'''
+
+'''arr = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
+
+print(arr[0, 1, 2])'''
+
+'''arr = np.array([1, 2, 3, 4], dtype='S')
+
+print(arr.dtype)'''
+
+'''arr = np.array([1, 2, 3, 4], dtype='i4')
+
+print(arr.dtype)'''
+
+'''arr = np.array(['apple', 'banana', 'cherry'])
+
+print(arr.dtype)'''
+
